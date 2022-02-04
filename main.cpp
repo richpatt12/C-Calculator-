1e7fc57a96ad5e84a166a370210894362050c2f3
Richard Patterson <67016335+richpatt12@users.noreply.github.com>
2022-02-04 12:20:21 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,58 +1,81 @@
 #include <iostream>
 
 using namespace std;
-//Declares variable option,num1,num2, and sum
-int option;//variable to store users selected option
-int num1;//variable to store number 1 of user
-int num2;//variable to store number 2
-int sum;//variable to store the sum of number 1 and 2
-void addition (){ // addition function
-    sum = num1 + num2;
-    cout <<sum;
-    
-}
-void subtraction (){// subtraction function
-    sum = num1 - num2;
-    cout <<sum;
-    
-}
-void multiply (){// multiplication function
-    sum = num1 * num2;
-    cout <<sum;
-    
-}
-void divide (){// divide function
-    sum = num1 / num2;
-    cout <<sum;
-    
-}
-void exit () {// divide function
-    cout <<"Thanks for at least trying. "<<endl;
-    return 0;
+//Variables Declared
+int option;			
+int get_Num1;			
+int get_Num2;			
+int get_Sum;			
+//Operation Functions
+void
+addition ()
+{				
+  get_Sum = get_Num1 + get_Num2;
+  cout << get_Sum;
+
 }
 
-int main() {
- cout <<"Welcome to your calculator. Please select a option from below. "<<endl;
- cout<<"1.Addition, 2.Subtraction, 3.Divide, 4.Multiply or 5.To close program"<<endl;
- cin >> option;
-cout << "enter your first number: ";
-cin >> num1;
-cout <<"enter your second number: ";
-cin >> num2;
-if (option == 1)
+void
+subtraction ()
+{				
+  get_Sum = get_Num1 - get_Num2;
+  cout << get_Sum;
+
+}
+
+void
+multiply ()
+{				
+  get_Sum = get_Num1 * get_Num2;
+  cout << get_Sum;
+
+}
+
+void
+divide ()
+{				
+  get_Sum = get_Num1 / get_Num2;
+  cout << get_Sum;
+
+}
+//Option Function
+void
+operations ()
 {
-    addition();
-} else if(option ==2){
-    subtraction();
+  switch (option)
+    {
+    case 1:
+      addition ();
+
+      break;
+    case 2:
+      subtraction ();
+      break;
+    case 3:
+      divide ();
+      break;
+    case 4:
+      multiply ();
+      break;
+    }
 }
-else if(option ==3){
-    divide();
-}
-else if(option ==4){
-    multiply();
-}
-else if (option ==5){
-    exit()
-}
+
+int
+main ()
+{
+    //Ask user for options and saves it
+  cout << "Welcome to your calculator. Please select a option from below. " <<endl;
+
+  cout <<"1.Addition, 2.Subtraction, 3.Divide, 4.Multiply or 5.To close program" <<endl;
+
+  cin >> option;
+//Ask user for number inputs
+  cout << "enter your first number: ";
+  cin >> get_Num1;
+  cout << "enter your second number: ";
+  cin >> get_Num2;
+  operations ();
+
+
   return 0;
 }
